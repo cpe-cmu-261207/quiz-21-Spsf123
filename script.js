0342eb389c6a3a70234aff58e8ad63df5d37a5ea
Surapa <surapa152@gmail.com>
2022-08-04 14:19:56 +0700

    hihi

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+callApi();
+
+const gen = document.getElementById("span-gender-icon");
+const img = document.getElementById("img-profile");
+const nametag = document.getElementById("p-name");
+const address = document.getElementById("p-address");
+const email = document.getElementById("p-email");
+const btnRandom = document.getElementById("btn-random");
+
+btnRandom.onclick = () => {
+  callApi();
+};
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  if (resp.data.results[0].gender == "female") {
+    gen.innerText = "👩";
+  } else {
+    gen.innerText = "👨";
+  }
+  img.src = resp.data.results[0].picture.large;
+  nametag.innerText =
+    resp.data.results[0].name.first + " " + resp.data.results[0].name.last;
+  email.innerText = resp.data.results[0].email;
+  address.innerText =
+    resp.data.results[0].location.city +
+    " " +
+    resp.data.results[0].location.state +
+    " " +
+    resp.data.results[0].location.country +
+    " " +
+    resp.data.results[0].location.postcode;
+  console.log(resp.data.results[0]);
 }
